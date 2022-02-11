b072a65bd6f85377f7b6331cda93ae89f42e0f1a
marion21 <marion.coudrais@live.fr>
2022-02-11 17:04:40 +0100

    migrations ok

--- GestionnaireProjet/models.py
+++ GestionnaireProjet/models.py
@@ -1,3 +1,53 @@
+from datetime import timezone,timedelta,datetime
+
+import django
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):  # class abstraite
+
+    lastName = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.firstName + ' ' + self.lastName
+
+
+class ProjectManager(Employee):  # hérite de Employee
+    pass
+
+
+class Operator(Employee):  # hérite de Employee
+    pass
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=100)
+    deliveryDate = models.DateField(default=django.utils.timezone.now)
+    creationDate = models.DateField(default=django.utils.timezone.now)
+    status = models.CharField(max_length=50)
+    responsible = models.ForeignKey(ProjectManager, on_delete=models.CASCADE)
+
+
+# class ProjectTask(models.Model):
+#     id =
+#     idProject =
+#     idTask =
+
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+    estimatedTime = models.IntegerField(default=1)
+    startDate = models.DateField(default=django.utils.timezone.now)
+    priority = models.IntegerField(default=0)
+    project = models.ForeignKey(Project, default=0, on_delete=models.CASCADE)
+    responsible = models.ForeignKey(ProjectManager, on_delete=models.CASCADE)
+
